a9504704147b65710e140130eafbf4343d8dda1b
sainath <allussainathreddy@gmail.com>
2019-11-06 10:00:56 +0530

    Add Color Helper to generate Colors and slider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors'
 import './App.css';
+import { generatePalette } from './colorHelpers'
 
-function App() {
+class App extends Component{
+  render(){
+    console.log(generatePalette(seedColors[4]))
   return (
     <div>
-     <Palette {...seedColors[4]}/>
+     <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
-
+}
 export default App;
